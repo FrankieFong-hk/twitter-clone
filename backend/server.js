d9cf6eae96497173882b4d200a7a3891875be169
FrankieFong-hk <httfong@gmail.com>
2025-05-08 17:03:12 -0400

    feat: configure production deployment with Express static serving and HTTPS URL handling

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
 import notificationRoutes from "./routes/notification.route.js";
 import connectMongoDB from "./db/connectMongoDB.js";
+import path from "path";
 
 dotenv.config();
 
@@ -20,6 +21,8 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const __dirname = path.resolve();
+
 app.use(express.json({ limit: "10mb" })); // to parse req.body with increased size limit
 app.use(express.urlencoded({ extended: true })); // to parse form data with increased size limit
 
@@ -29,6 +32,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
